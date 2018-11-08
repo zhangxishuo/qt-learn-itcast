22af9bd8cc283349055ba5ecd16eaaccd8d10448
zhangxishuo <zhangxishuo1998@gmail.com>
2018-11-08 23:27:00 +0800

    connect

--- SignalAndSlot/mainwidget.cpp
+++ SignalAndSlot/mainwidget.cpp
@@ -9,8 +9,16 @@ MainWidget::MainWidget(QWidget *parent)
 
     // 指针需要分配空间
     ptrButton = new QPushButton(this);
-    ptrButton->setText(QString("(ಡωಡ) ptr"));
+    ptrButton->setText(QString("Close Widget"));
     ptrButton->move(50, 50);
+
+    /*
+     * ptrButton              信号发出者，指针类型
+     * &QPushButton::pressed  信号类型
+     * this                   信号接收者
+     * &MainWidget::close     槽(信号处理)函数
+     */
+    connect(ptrButton, &QPushButton::pressed, this, &MainWidget::close);
 }
 
 MainWidget::~MainWidget()
